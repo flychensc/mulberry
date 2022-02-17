75f064a0f40cee176344924b09bf0af702fb163f
陈世昌 <flychensc@live.com>
2022-02-17 19:28:51 +0800

    加入MA效果还满意

--- draw.py
+++ draw.py
@@ -15,13 +15,13 @@ my_style = mpf.make_mpf_style(marketcolors=my_color)
 FOLDER = "img"
 
 
-def gen_kline(data, fpath):
+def gen_kline(data, fpath, mav):
     data = pd.DataFrame(data)
     data['datetime'] = data['datetime'].map(lambda x: dt.datetime.strptime(str(x), "%Y-%m-%d").date())
     data.index = pd.to_datetime(data.datetime)
     data = data.drop(columns=['datetime'])
 
-    mpf.plot(data, type='candle', volume=True, style=my_style, axisoff=True, tight_layout=True, scale_padding=0, savefig={'fname': fpath})
+    mpf.plot(data, type='candle', mav=mav, volume=True, style=my_style, axisoff=True, tight_layout=True, scale_padding=0, savefig={'fname': fpath})
 
 
 def bluk():
@@ -33,6 +33,7 @@ def bluk():
         p.mkdir(parents=True)
 
     PERIOD = config.getint('CANDLE', 'PERIOD')
+    mav = (config.getint('MA', 'MA1'), config.getint('MA', 'MA2'), config.getint('MA', 'MA3'))
 
     symbol_list = config.get('PROB', 'ID_LIST').split(',')
 
@@ -52,7 +53,7 @@ def bluk():
         if len(stock_zh_a_hist_df) < PERIOD:
             print("length error on ", symbol)
         else:
-            gen_kline(stock_zh_a_hist_df[-PERIOD:], fpath)
+            gen_kline(stock_zh_a_hist_df[-PERIOD:], fpath, mav)
 
 
 if __name__ == "__main__":
